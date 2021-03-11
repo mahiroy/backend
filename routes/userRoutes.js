a15532d0ad9e4b53542562f9d444455a8d217818
mahiroy <mahiroy101097@gmail.com>
2021-03-12 00:25:04 +0530

    ad

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,27 +1,27 @@
 // unclude library
- 
+
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
-const {check, validationResult, body} = require('express-validator');
+const { check, validationResult, body } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const moment = require('moment');
 
 const User = require('./../models/User');
 const token_key = process.env.TOKEN_KEY;
- 
+
 
 // middleware setup
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.urlencoded({ extended: true }));
 
- 
+
 //default rout
 //Access: public
 // url http://localhot:500/api/users/
 // method:GET
 router.get(
-    '/', (req,res) =>{
+    '/', (req, res) => {
         return res.status(200).json(
             {
                 "status": true,
@@ -34,7 +34,7 @@ router.get(
 //Access: public
 // url http://localhot:500/api/user/register 
 // method:POST
-router.post( 
+router.post(
     '/register',
     [
         //check empty field
@@ -43,27 +43,63 @@ router.post(
 
         //check email
         check('email').isEmail().normalizeEmail()
-    ], 
-    (req,res) =>{
+    ],
+    (req, res) => {
         const errors = validationResult(req);
         //check error is not empty
-        if(!errors.isEmpty()){
-             return res.status(400).json({
-                 "status": false,
-                 "errors" : errors.array()
-             })
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                "status": false,
+                "errors": errors.array()
+            })
         }
 
- 
-        const salt = bcrypt.genSaltSync(10)
-        const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+
         
-        return res.status(200).json({
-            "status":true,
-            "data":req.body,
-            "hashedPassword": hashedPassword
-        });
+        User.findOne({ email: req.body.email }).then(user => {
+           //user email exist or not
+            if (user) {
+                return res.status(409).json({
+                    "status": false,
+                    "message": "user email alredy exist"
+                });
+            }
+            else{
+
+                const salt = bcrypt.genSaltSync(10)
+                const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+                
+                const newUser = new User({
+                    email: req.body.email,
+                    username: req.body.username,
+                    password:hashedPassword
+                });
+
+                newUser.save().then(result =>{
+                    return res.status(200).json({
+                        "status": true,
+                        "user": result
+                    });
+                }).catch(error =>{
+                    return res.status(502).json({
+                        "status": false,
+                        "user": error
+                    });
+                })
+            }
+        }).catch(error =>{
+            return res.status(502).json({
+                "status": false,
+                "user": error
+            });
+        })
+
+
+
+       
     });
 
 
- module.exports = router;
+module.exports = router;
